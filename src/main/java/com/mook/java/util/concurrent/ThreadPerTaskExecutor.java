3783f13045e10f0898dabcc3dde29b403e0d1dc1
maojunkai <maojunkai@youzan.com>
2018-12-02 16:51:07 +0800

    java.util.concurrent.Executor

--- src/main/java/com/mook/java/util/concurrent/ThreadPerTaskExecutor.java
+++ src/main/java/com/mook/java/util/concurrent/ThreadPerTaskExecutor.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executor;
 /**
  * @Author: maojunkai
  * @Date: 2018/11/30 上午12:31
- * @Description:
+ * @Description: 通过新建线程执行提交的任务：一个任务对应一个线程
  */
 public class ThreadPerTaskExecutor implements Executor {
     @Override
